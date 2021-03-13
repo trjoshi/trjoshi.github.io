2e1ed210403723a3935d9335751bc890f07a63f9
trjoshi <79432497+trjoshi@users.noreply.github.com>
2021-03-13 17:59:23 -0600

    p

--- WAPProject/WebContent/JavaScripts/maze.js
+++ WAPProject/WebContent/JavaScripts/maze.js
@@ -2,14 +2,15 @@ $(document).ready(function() {
 
 	$("#start").click(function() {
 		$(".boundary").removeClass("youlose");
-		$("#status").text("Click the S to begin.");
+		$("#status").text("Game Started");
 
 		// event for mouse enter in the div section
 
 		$(".boundary").mouseenter(function() {
 			$(".boundary").addClass("youlose");
 			// alert("Sorry, you lost :[");
-			$("#status").text("Sorry, you lost :[");
+			$("#status").text("Sorry, you lost :[ Please press S to restart the game.");
+			return false;
 		});
 
 		// event for mouse leave in the div section
@@ -17,27 +18,30 @@ $(document).ready(function() {
 		$("#maze").mouseleave(function() {
 			$(".boundary").addClass("youlose");
 			// alert("Sorry, you lost :[");
-			$("#status").text("Sorry, you lost :[");
+			$("#status").text("Sorry, you lost :[ Please press S to restart the game.");
+			return false;
 		});
 
 		// event for mouse end
 
 		$("#end").click(function() {
-			// alert("You win! :]");
-			$("#status").text("You win! :]");
-
-			$(".boundary").mouseenter(function() {
-				$(".boundary").removeClass("youlose");
-				// alert("Sorry, you lost :[");
+			if ($("#status").text() == "Game Started") {
+				// alert("You win! :]");
 				$("#status").text("You win! :]");
-			});
-			$("#maze").mouseleave(function() {
-				$(".boundary").removeClass("youlose");
-				// alert("Sorry, you lost :[");
-				$("#status").text("You win! :]");
-			});
 
+				$(".boundary").mouseenter(function() {
+					$(".boundary").removeClass("youlose");
+					// alert("Sorry, you lost :[");
+					$("#status").text("You win! :]");
+				});
+				$("#maze").mouseleave(function() {
+					$(".boundary").removeClass("youlose");
+					// alert("Sorry, you lost :[");
+					$("#status").text("You win! :]");
+				});
+			}
 		});
+
 	});
 
 });
